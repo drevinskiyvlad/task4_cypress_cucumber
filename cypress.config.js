401d5cedccb7ebe8972d4c31f86240bd1e279c9c
Drevinskyi Vladislav <drevinskiy.vlad@gmail.com>
2023-11-14 00:49:45 +0200

    added reporter

--- cypress.config.js
+++ cypress.config.js
@@ -4,23 +4,21 @@ const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-pr
 const esbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
 module.exports = defineConfig({
+  projectId: '7ienfa',
   e2e: {
     specPattern: "cypress/e2e/features/*.feature",
     baseUrl: "https://www.saucedemo.com/",
     chromeWebSecurity: false,
+
     async setupNodeEvents(on, config) {
       await addCucumberPreprocessorPlugin(on, config);
-
-      // Assuming esbuildPlugin is an object with a `name` property
       const preprocessor = esbuildPlugin.default || esbuildPlugin;
-
       on(
           "file:preprocessor",
           createBundler({
             plugins: [preprocessor(config)],
           })
       );
-
       return config;
     },
   },
