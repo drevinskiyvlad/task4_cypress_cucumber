bdc3337ebd5f80bf8acb727876661739d2126d4d
Drevinskyi Vladislav <drevinskiy.vlad@gmail.com>
2023-11-13 14:07:33 +0200

    added faker to generate random string

---
+++ cypress/helper/faker.js
@@ -0,0 +1,11 @@
+exports.Faker = class Faker {
+    static generateRandomString(length) {
+        const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        let result = '';
+        for (let i = 0; i < length; i++) {
+            const randomIndex = Math.floor(Math.random() * characters.length);
+            result += characters.charAt(randomIndex);
+        }
+        return result;
+    }
+}
